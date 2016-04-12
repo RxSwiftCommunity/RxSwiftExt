330a1cbdb0b0b202b45717332175909f9ca239e2
Marin Todorov <marin@underplot.com>
2016-04-12 15:51:30 +0200

    * improved unwrap playground examples

--- Demo/RxSwiftExtDemo/RxSwiftExtPlayground.playground/Pages/unwrap.xcplaygroundpage/Contents.swift
+++ Demo/RxSwiftExtDemo/RxSwiftExtPlayground.playground/Pages/unwrap.xcplaygroundpage/Contents.swift
@@ -8,19 +8,34 @@ import RxSwift_Ext
  ## unwrap
  
  The `unwrap` operator takes a sequence of optional elements
- and returns a sequence of non-optional elements, filtering out `nil` values.
+ and returns a sequence of non-optional elements, filtering out any `nil` values.
  */
-
-let numbers = [1, 2, nil, Int?(4)]
-
-numbers.toObservable()
-    .unwrap()
-    .toArray()
-    .subscribeNext { result in
-        // look types on the right panel ===>
-        numbers
-        result
-        print("unwrap() transformed \(numbers) to \(result)")
+example("unwrap optional values") {
+    
+    let numbers = Array<Int?>([1, 2, 3])
+    numbers.toObservable()
+        .unwrap()
+        .toArray()
+        .subscribeNext {result in
+            // look types on the right panel ===>
+            numbers
+            result
+            print("unwrap() transformed \(numbers) to \(result)")
     }
+}
+
+example("unwrap and filter out nil values") {
+    
+    let numbers = [1, 2, nil, Int?(4)]
+    numbers.toObservable()
+        .unwrap()
+        .toArray()
+        .subscribeNext { result in
+            // look types on the right panel ===>
+            numbers
+            result
+            print("unwrap() transformed \(numbers) to \(result)")
+    }
+}
 
 //: [Next](@next)
