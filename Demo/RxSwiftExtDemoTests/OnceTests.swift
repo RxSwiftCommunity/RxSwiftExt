b98da0d9497b2d9062136a0909c74f701aa8f8bb
Florent Pillet <fpillet@gmail.com>
2016-04-14 23:19:17 +0200

    more tests for `once`

--- Demo/RxSwiftExtDemoTests/OnceTests.swift
+++ Demo/RxSwiftExtDemoTests/OnceTests.swift
@@ -37,5 +37,46 @@ class OnceTests: XCTestCase {
         
         XCTAssertEqual(observer1.events, correct1)
         XCTAssertEqual(observer2.events, correct2)
+
+    }
+    
+    func testMultipleOnce() {
+        
+        let onceObservable1 = Observable.once("Hello")
+        let onceObservable2 = Observable.once("world")
+        let scheduler = TestScheduler(initialClock: 0)
+
+        let observer1 = scheduler.createObserver(String)
+        onceObservable1.subscribe(observer1)
+        let observer2 = scheduler.createObserver(String)
+        onceObservable1.subscribe(observer2)
+
+        let observer3 = scheduler.createObserver(String)
+        onceObservable2.subscribe(observer3)
+        let observer4 = scheduler.createObserver(String)
+        onceObservable2.subscribe(observer4)
+
+        scheduler.start()
+        
+        let correct1 = [
+            next(0, "Hello"),
+            completed(0)
+        ]
+        let correct2 : [Recorded<Event<String>>] = [
+            completed(0)
+        ]
+        let correct3 = [
+            next(0, "world"),
+            completed(0)
+        ]
+        let correct4 : [Recorded<Event<String>>] = [
+            completed(0)
+        ]
+
+        XCTAssertEqual(observer1.events, correct1)
+        XCTAssertEqual(observer2.events, correct2)
+        XCTAssertEqual(observer3.events, correct3)
+        XCTAssertEqual(observer4.events, correct4)
+
     }
 }
