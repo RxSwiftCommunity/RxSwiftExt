a84b514d5454217dac533325547f8ce98e71e0f3
Anton Efimenko <reloni@ya.ru>
2016-07-18 16:56:53 +0300

    More tests, fixes and comments

--- Demo/RxSwiftExtDemoTests/RetryWithBehaviorTests.swift
+++ Demo/RxSwiftExtDemoTests/RetryWithBehaviorTests.swift
@@ -68,7 +68,7 @@ class RetryWithBehaviorTests: XCTestCase {
 			error(690, RepeatTestErrors.fatalError)]
 		
 		let res = scheduler.start(0, subscribed: 0, disposed: 1000) { () -> Observable<Int> in
-			return self.sampleValues.asObservable().retry(.Immediate(maxAttemptCount: 3), scheduler: self.scheduler) { error in
+			return self.sampleValues.asObservable().retry(.Immediate(maxAttemptCount: 3), scheduler: self.scheduler) { _ in
 				return true
 			}
 		}
@@ -83,7 +83,7 @@ class RetryWithBehaviorTests: XCTestCase {
 			error(230, RepeatTestErrors.fatalError)]
 		
 		let res = scheduler.start(0, subscribed: 0, disposed: 1000) { () -> Observable<Int> in
-			return self.sampleValues.asObservable().retry(.Immediate(maxAttemptCount: 3), scheduler: self.scheduler) { error in
+			return self.sampleValues.asObservable().retry(.Immediate(maxAttemptCount: 3), scheduler: self.scheduler) { _ in
 				return false
 			}
 		}
@@ -97,9 +97,9 @@ class RetryWithBehaviorTests: XCTestCase {
 			next(220, 2),
 			next(445, 1),
 			next(455, 2),
-			next(685, 1),
-			next(695, 2),
-			error(705, RepeatTestErrors.fatalError)]
+			next(680, 1),
+			next(690, 2),
+			error(700, RepeatTestErrors.fatalError)]
 		
 		let res = scheduler.start(0, subscribed: 0, disposed: 1000) { () -> Observable<Int> in
 			return self.sampleValues.asObservable().retry(.Delayed(maxAttemptCount: 3, time: 5.0), scheduler: self.scheduler)
@@ -108,4 +108,178 @@ class RetryWithBehaviorTests: XCTestCase {
 		XCTAssertEqual(res.events, correctValues)
 	}
 
+	func testDelayedRetryWithPredicate() {
+		let correctValues = [
+			next(210, 1),
+			next(220, 2),
+			next(445, 1),
+			next(455, 2),
+			next(680, 1),
+			next(690, 2),
+			error(700, RepeatTestErrors.fatalError)]
+		
+		let res = scheduler.start(0, subscribed: 0, disposed: 1000) { () -> Observable<Int> in
+			return self.sampleValues.asObservable().retry(.Delayed(maxAttemptCount: 3, time: 5.0), scheduler: self.scheduler) { _ in
+				return true
+			}
+		}
+		
+		XCTAssertEqual(res.events, correctValues)
+	}
+	
+	func testDelayedNotRetryWithPredicate() {
+		let correctValues = [
+			next(210, 1),
+			next(220, 2),
+			error(230, RepeatTestErrors.fatalError)]
+		
+		let res = scheduler.start(0, subscribed: 0, disposed: 1000) { () -> Observable<Int> in
+			return self.sampleValues.asObservable().retry(.Delayed(maxAttemptCount: 3, time: 5.0), scheduler: self.scheduler) { _ in
+				return false
+			}
+		}
+		
+		XCTAssertEqual(res.events, correctValues)
+	}
+	
+	func testExponentialRetryWithoutPredicate() {
+		let correctValues = [
+			next(210, 1),
+			next(220, 2),
+			next(445, 1),
+			next(455, 2),
+			next(685, 1),
+			next(695, 2),
+			next(935, 1),
+			next(945, 2),
+			error(955, RepeatTestErrors.fatalError)]
+		
+		let res = scheduler.start(0, subscribed: 0, disposed: 1000) { () -> Observable<Int> in
+			return self.sampleValues.asObservable().retry(.ExponentialDelayed(maxAttemptCount: 4, initial: 5.0, multiplier: 1.0), scheduler: self.scheduler)
+		}
+		
+		XCTAssertEqual(res.events, correctValues)
+	}
+	
+	func testExponentialRetryWithPredicate() {
+		let correctValues = [
+			next(210, 1),
+			next(220, 2),
+			next(445, 1),
+			next(455, 2),
+			next(685, 1),
+			next(695, 2),
+			next(935, 1),
+			next(945, 2),
+			error(955, RepeatTestErrors.fatalError)]
+		
+		let res = scheduler.start(0, subscribed: 0, disposed: 1000) { () -> Observable<Int> in
+			return self.sampleValues.asObservable().retry(.ExponentialDelayed(maxAttemptCount: 4, initial: 5.0, multiplier: 1.0), scheduler: self.scheduler) { _ in
+				return true
+			}
+		}
+		
+		XCTAssertEqual(res.events, correctValues)
+	}
+	
+	func testExponentialNotRetryWithPredicate() {
+		let correctValues = [
+			next(210, 1),
+			next(220, 2),
+			error(230, RepeatTestErrors.fatalError)]
+		
+		let res = scheduler.start(0, subscribed: 0, disposed: 1000) { () -> Observable<Int> in
+			return self.sampleValues.asObservable().retry(.ExponentialDelayed(maxAttemptCount: 4, initial: 5.0, multiplier: 1.0), scheduler: self.scheduler) { _ in
+				return false
+			}
+		}
+		
+		XCTAssertEqual(res.events, correctValues)
+	}
+	
+	func testCustomTimerRetryWithoutPredicate() {
+		let correctValues = [
+			next(210, 1),
+			next(220, 2),
+			next(450, 1),
+			next(460, 2),
+			next(710, 1),
+			next(720, 2),
+			next(990, 1),
+			next(1000, 2),
+			next(1300, 1),
+			next(1310, 2),
+			error(1320, RepeatTestErrors.fatalError)]
+		
+		let res = scheduler.start(0, subscribed: 0, disposed: 2000) { () -> Observable<Int> in
+			let customCalculator: (UInt -> Double) = { attempt in
+				switch attempt {
+				case 1: return 10.0
+				case 2: return 30.0
+				case 3: return 50.0
+				default: return 80.0
+				}
+			}
+			
+			return self.sampleValues.asObservable().retry(.CustomTimerDelayed(maxAttemptCount: 5, delayCalculator: customCalculator), scheduler: self.scheduler)
+		}
+		
+		XCTAssertEqual(res.events, correctValues)
+	}
+	
+	func testCustomTimerRetryWithPredicate() {
+		let correctValues = [
+			next(210, 1),
+			next(220, 2),
+			next(450, 1),
+			next(460, 2),
+			next(710, 1),
+			next(720, 2),
+			next(990, 1),
+			next(1000, 2),
+			next(1300, 1),
+			next(1310, 2),
+			error(1320, RepeatTestErrors.fatalError)]
+		
+		let res = scheduler.start(0, subscribed: 0, disposed: 2000) { () -> Observable<Int> in
+			let customCalculator: (UInt -> Double) = { attempt in
+				switch attempt {
+				case 1: return 10.0
+				case 2: return 30.0
+				case 3: return 50.0
+				default: return 80.0
+				}
+			}
+			
+			return self.sampleValues.asObservable().retry(.CustomTimerDelayed(maxAttemptCount: 5, delayCalculator: customCalculator), scheduler: self.scheduler) { _ in
+				return true
+			}
+		}
+		
+		XCTAssertEqual(res.events, correctValues)
+	}
+	
+	func testCustomTimerNotRetryWithPredicate() {
+		let correctValues = [
+			next(210, 1),
+			next(220, 2),
+			error(230, RepeatTestErrors.fatalError)]
+		
+		let res = scheduler.start(0, subscribed: 0, disposed: 2000) { () -> Observable<Int> in
+			let customCalculator: (UInt -> Double) = { attempt in
+				switch attempt {
+				case 1: return 10.0
+				case 2: return 30.0
+				case 3: return 50.0
+				default: return 80.0
+				}
+			}
+			
+			return self.sampleValues.asObservable().retry(.CustomTimerDelayed(maxAttemptCount: 5, delayCalculator: customCalculator), scheduler: self.scheduler) { _ in
+				return false
+			}
+		}
+		
+		XCTAssertEqual(res.events, correctValues)
+	}
 }
--- Source/retryWithBehavior.swift
+++ Source/retryWithBehavior.swift
@@ -9,6 +9,13 @@
 import Foundation
 import RxSwift
 
+/**
+Specifies how observable sequence will be repeated in case of an error
+- Immediate: Will be immediatelly repeated specified number of times
+- Delayed:  Will be repeated after specified delay specified number of times
+- ExponentialDelayed: Will be repeated specified number of times. Delay will be increased after each iteration
+- CustomTimerDelayed: Will be repeated specified number of times. Delay will be calculated by custom closure
+*/
 public enum RepeatBehavior {
 	case Immediate (maxAttemptCount: UInt)
 	case Delayed (maxAttemptCount: UInt, time: Double)
@@ -18,46 +25,78 @@ public enum RepeatBehavior {
 
 public typealias RepeatPredicate = (ErrorType) -> Bool
 
+extension RepeatBehavior {
+	/**
+	Extracts maxAttemptCount and calculates delay for current RepeatBehavior
+	- parameter currentAttempt: Number of current attempt
+	- returns: Tuple with maxAttemptCount and calculated delay for provided attempt
+	*/
+	func calculateConditions(currentAttempt: UInt) -> (maxAttemptCount: UInt, delay: Double) {
+		switch self {
+		case .Immediate(let max):
+			// if Immediate, return 0.0 as delay
+			return (maxAttemptCount: max, delay: 0.0)
+		case .Delayed(let max, let time):
+			// return specified delay
+			return (maxAttemptCount: max, delay: time)
+		case .ExponentialDelayed(let max, let initial, let multiplier):
+			// if it's first attempt, simply use initial delay, otherwise calculate delay
+			let delay = currentAttempt == 1 ? initial : initial * pow(1 + multiplier, Double(currentAttempt - 1))
+			return (maxAttemptCount: max, delay: delay)
+		case .CustomTimerDelayed(let max, let delayCalculator):
+			// calculate delay using provided calculator
+			return (maxAttemptCount: max, delay: delayCalculator(currentAttempt))
+		}
+	}
+}
+
 extension ObservableType {
-	// retry with behavior on .Error. Complete on .Completed
+	/**
+	Repeats the source observable sequence using given behavior in case of an error or until it successfully terminated
+	- parameter behavior: Behavior that will be used in case of an error
+	- parameter scheduler: Schedular that will be used for delaying subscription after error
+	- parameter shouldRetry: Custom optional closure for checking error (if returns true, repeat will be performed)
+	- returns: Observable sequence that will be automatically repeat if error occurred
+	*/
+	@warn_unused_result(message="http://git.io/rxs.uo")
 	public func retry(behavior: RepeatBehavior, scheduler : SchedulerType = MainScheduler.instance, shouldRetry : RepeatPredicate? = nil) -> Observable<E> {
-		return retry(behavior, attemptCounter: 1, scheduler: scheduler, shouldRetry: shouldRetry)
+		return retry(1, behavior: behavior, scheduler: scheduler, shouldRetry: shouldRetry)
 	}
 	
-	internal func retry(behavior: RepeatBehavior, attemptCounter: UInt = 0, scheduler : SchedulerType = MainScheduler.instance, shouldRetry : RepeatPredicate? = nil)
+	/**
+	Repeats the source observable sequence using given behavior in case of an error or until it successfully terminated
+	- parameter currentAttempt: Number of current attempt
+	- parameter behavior: Behavior that will be used in case of an error
+	- parameter scheduler: Schedular that will be used for delaying subscription after error
+	- parameter shouldRetry: Custom optional closure for checking error (if returns true, repeat will be performed)
+	- returns: Observable sequence that will be automatically repeat if error occurred
+	*/
+	@warn_unused_result(message="http://git.io/rxs.uo")
+	internal func retry(currentAttempt: UInt, behavior: RepeatBehavior, scheduler : SchedulerType = MainScheduler.instance, shouldRetry : RepeatPredicate? = nil)
 		-> Observable<E> {
-			guard attemptCounter > 0 else { return Observable.empty() }
-
-			var delay: Double!
-			let maxAttemptCount: UInt!
+			guard currentAttempt > 0 else { return Observable.empty() }
 			
-			switch behavior {
-			case .Immediate(let max):
-				maxAttemptCount = max
-				delay = 0
-			case .Delayed(let max, let time):
-				maxAttemptCount = max
-				delay = time
-			case .ExponentialDelayed(let max, let initial, let multiplier):
-				maxAttemptCount = max
-				// if it's first attempt, simply use initial delay, otherwise calculate delay
-				delay = attemptCounter == 1 ? initial : initial * pow(1 + multiplier, Double(attemptCounter))
-			case .CustomTimerDelayed(let max, let delayCalculator):
-				maxAttemptCount = max
-				delay = delayCalculator(attemptCounter)
-			}
+			// calculate conditions for bahavior
+			let conditions = behavior.calculateConditions(currentAttempt)
 			
 			return catchError { error -> Observable<E> in
-				guard maxAttemptCount > attemptCounter else { return Observable.error(error) }
+				// return error if exceeds maximum amount of retries
+				guard conditions.maxAttemptCount > currentAttempt else { return Observable.error(error) }
+				
 				if let shouldRetry = shouldRetry where !shouldRetry(error) {
+					// also return error if predicate says so
 					return Observable.error(error)
 				}
-				
-				if delay > 0 {
-					return self.delaySubscription(delay, scheduler: scheduler).retry(behavior, attemptCounter: attemptCounter + 1, scheduler: scheduler, shouldRetry: shouldRetry)
+
+				guard conditions.delay > 0.0 else {
+					// if there is no delay, simply retry
+					return self.retry(currentAttempt + 1, behavior: behavior, scheduler: scheduler, shouldRetry: shouldRetry)
 				}
 				
-				return self.retry(behavior, attemptCounter: attemptCounter + 1, scheduler: scheduler, shouldRetry: shouldRetry)
+				// otherwise retry after specified delay
+				return Observable<Void>.just().delaySubscription(conditions.delay, scheduler: scheduler).flatMapLatest {
+					self.retry(currentAttempt + 1, behavior: behavior, scheduler: scheduler, shouldRetry: shouldRetry)
+				}
 			}
 	}
 }
