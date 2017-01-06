ecb7663aa653c29c92aa33518f3a4dac546e162e
Andy Chou <acchou2@gmail.com>
2017-01-05 20:49:19 -0800

    Clean up playground example to be clearer about the nested events that materialize creates.

--- Playground/RxSwiftExtPlayground.playground/Pages/materialize.xcplaygroundpage/Contents.swift
+++ Playground/RxSwiftExtPlayground.playground/Pages/materialize.xcplaygroundpage/Contents.swift
@@ -23,51 +23,41 @@ import RxSwiftExt
  - seealso: [materialize operator on reactivex.io](http://reactivex.io/documentation/operators/materialize-dematerialize.html)
  */
 example("materialize a sequence") {
-
     let numbers = [1, 2, 3]
-
     print("materialize() transformed \(numbers) to sequence of Events: ")
-
     let materialized = Observable.from(numbers).materialize()
-
-    materialized.subscribe(onNext: { result in
+    materialized.subscribe{ result in
         print(result)
-    })
-
-    print()
-    print("...and dematerialize() transformed it back: ")
-
-    materialized.dematerialize().subscribe(onNext: { result in
+    }
+    print("\n...and dematerialize() transformed it back: ")
+    materialized.dematerialize().subscribe { result in
         print(result)
-    })
+    }
 }
 
 example("materialize errors") {
     enum MyErrors: Error {
         case AnError(input: Int)
     }
-
     // Simulate a network request with a 50% chance of failure
     func myRequest(input: Int) -> Observable<Int> {
-        if arc4random() % 2 == 0 { return Observable.just(input) }
+        if arc4random() % 2 == 0 {
+            return Observable.just(input)
+        }
         return Observable.error(MyErrors.AnError(input: input))
     }
-
     let materialized = Observable<Int>.range(start: 0, count: 10)
         .flatMap { input -> Observable<Event<Int>> in
             return myRequest(input: input).materialize()
-        }.publish()
-
-    // Be careful that for cold observables, each subscription to the materialized sequence results in the underlying sequences being regenerated. For hot observables that is not an issue.
-
+        }
+        .publish()
+     // For cold observables, each subscription to the materialized sequence results in the underlying sequences being regenerated. For hot observables that is not an issue. We use `publish` to turn this cold observable into a hot one.
     materialized.elements().subscribe(onNext: { result in
         print("Successful request: \(result)")
     })
-
     materialized.errors().subscribe(onNext: { result in
         print("Unsuccessful request: \(result)")
     })
-
     materialized.connect()
 }
 
