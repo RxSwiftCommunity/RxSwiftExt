033d1136ad542f37c695c47458e798cbf992beb7
Tanguy Helesbeux <tanguyhelesbeux@gmail.com>
2017-05-24 17:47:52 +0200

    Add tests for pausableBuffered limit parameter

--- Tests/RxSwift/pausableBufferedTests.swift
+++ Tests/RxSwift/pausableBufferedTests.swift
@@ -90,6 +90,79 @@ class PausableBufferedTests: XCTestCase {
         
     }
     
+    func testPausedLimit() {
+        
+        let underlying = scheduler.createHotObservable([
+            next(150, 1),
+            next(210, 2),
+            next(230, 3),
+            next(301, 4),
+            next(350, 5),
+            next(399, 6),
+            completed(500),
+            ])
+        
+        let pauser = scheduler.createHotObservable([
+            next(220, true),
+            next(300, false),
+            next(400, true),
+            ])
+        
+        let res = scheduler.start(1000) {
+            underlying.pausableBuffered(pauser, limit: 2)
+        }
+        
+        XCTAssertEqual(res.events, [
+            next(220, 2),
+            next(230, 3),
+            next(400, 5),
+            next(400, 6),
+            completed(500),
+            ])
+        
+        XCTAssertEqual(underlying.subscriptions, [
+            Subscription(200, 500),
+            ])
+        
+    }
+    
+    func testPausedNoLimit() {
+        
+        let underlying = scheduler.createHotObservable([
+            next(150, 1),
+            next(210, 2),
+            next(230, 3),
+            next(301, 4),
+            next(350, 5),
+            next(399, 6),
+            completed(500),
+            ])
+        
+        let pauser = scheduler.createHotObservable([
+            next(220, true),
+            next(300, false),
+            next(400, true),
+            ])
+        
+        let res = scheduler.start(1000) {
+            underlying.pausableBuffered(pauser, limit: nil)
+        }
+        
+        XCTAssertEqual(res.events, [
+            next(220, 2),
+            next(230, 3),
+            next(400, 4),
+            next(400, 5),
+            next(400, 6),
+            completed(500),
+            ])
+        
+        XCTAssertEqual(underlying.subscriptions, [
+            Subscription(200, 500),
+            ])
+        
+    }
+    
     func testPausedError() {
         
         let underlying = scheduler.createHotObservable([
