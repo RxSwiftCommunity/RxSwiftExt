d2bdb6aa3bb4edd93d553ec1edf00695a1339fa0
Joan Disho <joan.disho@quickbirdstudios.com>
2018-02-17 01:47:03 +0100

    create sort operator

---
+++ Source/RxSwift/sorted.swift
@@ -0,0 +1,35 @@
+//
+//  Sorted.swift
+//  RxSwiftExt
+//
+//  Created by Joan Disho on 17.02.18.
+//  Copyright © 2018 RxSwiftCommunity. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+extension Observable where E: Sequence, E.Iterator.Element: Comparable {
+    
+    /**
+     Transforms an observable of comparables into an observable of ordered arrays.
+     
+     - returns: The sorted observable.
+    */
+    
+    public func sorted<T>() -> Observable<T> where E.Iterator.Element == T {
+        return self.map { $0.sorted() }
+    }
+    
+    /**
+     Transforms an observable of comparables into an observable of ordered arrays by using the function passed in.
+     
+     - parameter areInIncreasingOrder: A function to compare to elements.
+     - returns: The sorted observable.
+     */
+    
+    public func sorted<T>(_ areInIncreasingOrder: @escaping (T, T) -> Bool) -> Observable<[T]> 
+        where E.Iterator.Element == T {
+        return self.map { $0.sorted(by: areInIncreasingOrder) }
+    }
+}
