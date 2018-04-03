650262d0aac495c0f650abd8c19fa1f8527df7ea
freak4pc <freak4pc@gmail.com>
2018-04-03 21:28:04 +0300

    Updated tests using RxTest

--- Tests/RxSwift/WithUnretainedTests.swift
+++ Tests/RxSwift/WithUnretainedTests.swift
@@ -10,89 +10,115 @@ import XCTest
 import RxSwift
 import RxTest
 
-private class Displayer {
-    let deinitAction: (() -> Void)?
+class WithUnretainedTests: XCTestCase {
+    fileprivate var testClass: TestClass!
+    var values: TestableObservable<Int>!
+    var tupleValues: TestableObservable<(Int, String)>!
+    let scheduler = TestScheduler(initialClock: 0)
 
-    init(deinitAction: (() -> Void)? = nil) {
-        self.deinitAction = deinitAction
+    override func setUp() {
+        super.setUp()
+
+        testClass = TestClass()
+        values = scheduler.createColdObservable([
+            .next(210, 1),
+            .next(215, 2),
+            .next(220, 3),
+            .next(225, 5),
+            .next(230, 8),
+            completed(250)
+        ])
+
+        tupleValues = scheduler.createColdObservable([
+            .next(210, (1, "a")),
+            .next(215, (2, "b")),
+            .next(220, (3, "c")),
+            .next(225, (5, "d")),
+            .next(230, (8, "e")),
+            completed(250)
+        ])
     }
 
-    deinit {
-        deinitAction?()
+    func testObjectAttached() {
+        let testClassId = testClass.id
+
+        let correctValues: [Recorded<Event<String>>] = [
+            .next(410, "\(testClassId), 1"),
+            .next(415, "\(testClassId), 2"),
+            .next(420, "\(testClassId), 3"),
+            .next(425, "\(testClassId), 5"),
+            .next(430, "\(testClassId), 8"),
+            completed(450)
+        ]
+
+        let res = scheduler.start {
+            self.values
+                .withUnretained(self.testClass)
+                .map { "\($0.0.id), \($0.1)" }
+        }
+
+        XCTAssertEqual(res.events, correctValues)
     }
 
-    func display(_ value: Any) {
-        print(value)
+    func testObjectDeallocates() {
+        _ = self.values
+                .withUnretained(self.testClass)
+                .subscribe()
+
+        // Confirm the object can be deallocated
+        XCTAssertTrue(testClass != nil)
+        testClass = nil
+        XCTAssertTrue(testClass == nil)
+    }
+
+    func testObjectDeallocatesSequenceCompletes() {
+        let testClassId = testClass.id
+
+        let correctValues: [Recorded<Event<String>>] = [
+            .next(410, "\(testClassId), 1"),
+            .next(415, "\(testClassId), 2"),
+            .next(420, "\(testClassId), 3"),
+            completed(425)
+        ]
+
+        let res = scheduler.start {
+            self.values
+                .withUnretained(self.testClass)
+                .do(onNext: { _, value in
+                    // Release the object in the middle of the sequence
+                    // to confirm it properly terminates the sequence
+                    if value == 3 {
+                        self.testClass = nil
+                    }
+                })
+                .map { "\($0.0.id), \($0.1)" }
+        }
+
+        XCTAssertEqual(res.events, correctValues)
+    }
+
+    func testResultsSelector() {
+        let testClassId = testClass.id
+
+        let correctValues: [Recorded<Event<String>>] = [
+            .next(410, "\(testClassId), 1, a"),
+            .next(415, "\(testClassId), 2, b"),
+            .next(420, "\(testClassId), 3, c"),
+            .next(425, "\(testClassId), 5, d"),
+            .next(430, "\(testClassId), 8, e"),
+            completed(450)
+        ]
+
+        let res = scheduler.start {
+            self.tupleValues
+                .withUnretained(self.testClass) { ($0, $1.0, $1.1) }
+                .map { "\($0.0.id), \($0.1), \($0.2)" }
+        }
+
+        XCTAssertEqual(res.events, correctValues)
     }
 }
 
-class WithUnretainedTests: XCTestCase {
-    func test_unretained_gets_deallocated() {
-        // Test constants
-        let valuesToBeObserved = [1, 2, 3, 5, 8]
-        let valuesToBeIgnored = [13, 21]
-
-        // Test flags
-        var displayerWasDeallocated = false
-        var valuesObserved = [Int]()
-
-        // Object to be safely unowned
-        var displayer: Displayer? = Displayer(deinitAction: {
-            displayerWasDeallocated = true
-        })
-
-        let publishSubject = PublishSubject<Int>()
-
-        if let displayer = displayer {
-            _ = publishSubject
-                .withUnretained(displayer) // -> Observable<(Displayer, Int)>
-                .subscribe(onNext: { displayer, i in
-                    displayer.display(i)
-                    valuesObserved.append(i)
-                })
-        }
-
-        valuesToBeObserved.forEach { publishSubject.onNext($0) }
-        displayer = nil
-        valuesToBeIgnored.forEach { publishSubject.onNext($0) }
-        publishSubject.onCompleted()
-
-        XCTAssertTrue(displayerWasDeallocated)
-        XCTAssertEqual(valuesObserved, valuesToBeObserved)
-    }
-
-    func test_sequence_completes() {
-        // Test constants
-        let valuesToBeObserved = [1, 2, 3, 5, 8]
-        let valuesToBeIgnored = [13, 21]
-
-        // Test flags
-        var outerSequenceHasCompleted = false
-        var valuesObserved = [Int]()
-
-        // Object to be safely unowned
-        var displayer: Displayer? = Displayer()
-
-        let publishSubject = PublishSubject<Int>()
-
-        if let displayer = displayer {
-            _ = publishSubject
-                .withUnretained(displayer) // -> Observable<(Displayer, Int)>
-                .debug()
-                .subscribe(onNext: { displayer, i in
-                    displayer.display(i)
-                    valuesObserved.append(i)
-                }, onError: nil,
-                   onCompleted: {
-                    outerSequenceHasCompleted = true
-                }, onDisposed: nil)
-        }
-
-        valuesToBeObserved.forEach { publishSubject.onNext($0) }
-        displayer = nil
-        valuesToBeIgnored.forEach { publishSubject.onNext($0) }
-
-        XCTAssertTrue(outerSequenceHasCompleted)
-        XCTAssertEqual(valuesObserved, valuesToBeObserved)
-    }
+private class TestClass {
+    let id: String = UUID().uuidString
 }
