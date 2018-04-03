475856e398035725d96ad79c4ce2af2354f020a6
freak4pc <freak4pc@gmail.com>
2018-04-03 20:48:40 +0300

    Clean up implementation and comments for withUnretained

--- Source/RxSwift/withUnretained.swift
+++ Source/RxSwift/withUnretained.swift
@@ -8,38 +8,44 @@
 
 import RxSwift
 
-private enum UnretainedError : Swift.Error {
-    case objectReleased
-}
-
 extension ObservableType {
     /**
-     Provides an unretained, safe to use (i.e. not implicitly unwrapped), reference to an object along with the values of the sequence
+     Provides an unretained, safe to use (i.e. not implicitly unwrapped), reference to an object along with the events emitted by the sequence.
+     In the case the provided object cannot be retained successfully, the seqeunce will complete.
      
      - parameter obj: The object to provide an unretained reference on.
      - parameter resultSelector: A function to combine the unretained referenced on `obj` and the value of the observable sequence.
      - returns: An observable sequence that contains the result of `resultSelector` being called with an unretained reference on `obj` and the values of the original sequence.
      */
-    public func withUnretained<T : AnyObject, Out>(_ obj: T, resultSelector: @escaping (T, Self.E) -> Out) -> Observable<Out> {
+    public func withUnretained<T: AnyObject, Out>(_ obj: T,
+                                                  resultSelector: @escaping (T, Self.E) -> Out) -> Observable<Out> {
         return map { [weak obj] element -> Out in
-            guard let obj = obj else { throw UnretainedError.objectReleased }
-            
+            guard let obj = obj else { throw UnretainedError.failedRetaining }
+
             return resultSelector(obj, element)
+        }
+        .catchError { error -> Observable<Out> in
+            guard let unretainedError = error as? UnretainedError,
+                  unretainedError == .failedRetaining else {
+                return .error(error)
             }
-            .catchError { error -> Observable<Out> in
-                if (error as? UnretainedError) == UnretainedError.objectReleased {
-                    return .empty()
-                }
-                return Observable.error(error)
+
+            return .empty()
         }
     }
+
     /**
-     Provides an unretained, safe to use (i.e. not implicitly unwrapped), reference to an object along with the values of the sequence
+     Provides an unretained, safe to use (i.e. not implicitly unwrapped), reference to an object along with the events emitted by the sequence.
+     In the case the provided object cannot be retained successfully, the seqeunce will complete.
      
      - parameter obj: The object to provide an unretained reference on.
      - returns: An observable sequence of tuples that contains both an unretained reference on `obj` and the values of the original sequence.
      */
-    public func withUnretained<T : AnyObject>(_ obj: T) -> Observable<(T, Self.E)> {
-        return withUnretained(obj, resultSelector: { obj, element in (obj, element) })
+    public func withUnretained<T: AnyObject>(_ obj: T) -> Observable<(T, Self.E)> {
+        return withUnretained(obj) { ($0, $1) }
     }
+}
+
+private enum UnretainedError: Swift.Error {
+    case failedRetaining
 }
