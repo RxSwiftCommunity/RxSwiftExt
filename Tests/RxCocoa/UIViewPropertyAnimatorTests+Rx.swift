62b3ae02f8bf763a19054d399830a245a7f45edc
freak4pc <freak4pc@gmail.com>
2018-04-04 13:16:21 +0300

    Minor test cleanup, moved availability clause to top of test to omit below iOS 10

--- Tests/RxCocoa/UIViewPropertyAnimatorTests+Rx.swift
+++ Tests/RxCocoa/UIViewPropertyAnimatorTests+Rx.swift
@@ -13,27 +13,25 @@ import RxSwiftExt
 import UIKit
 
 class UIViewPropertyAnimatorTests: XCTestCase {
-
     let disposeBag = DisposeBag()
 
+    @available(iOS 10.0, *)
     func testAnimationCompleted() {
-
         let expectations = expectation(description: "Animation completed")
 
         let view = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
-        if #available(iOS 10.0, *) {
-            let animator = UIViewPropertyAnimator(duration: 0.5, curve: .linear) {
-                view.transform = CGAffineTransform(translationX: 100, y: 100)
-            }
-            animator.rx.animate().subscribe(onCompleted: {
+        let animator = UIViewPropertyAnimator(duration: 0.5, curve: .linear) {
+            view.transform = CGAffineTransform(translationX: 100, y: 100)
+        }
+
+        animator
+            .rx.animate()
+            .subscribe(onCompleted: {
                 XCTAssertEqual(100, view.frame.origin.x)
                 XCTAssertEqual(100, view.frame.origin.y)
                 expectations.fulfill()
-            }).disposed(by: self.disposeBag)
-        } else {
-            // Fallback on earlier versions
-            expectations.fulfill()
-        }
+            })
+            .disposed(by: self.disposeBag)
 
         waitForExpectations(timeout: 1)
     }
