149fc766be29732ad8b3706e9f532af6dbf17c5b
freak4pc <freak4pc@gmail.com>
2018-04-04 13:13:59 +0300

    Minor updates to Playground page

--- Playground/RxSwiftExtPlayground.playground/Pages/UIViewPropertyAnimator+Rx.xcplaygroundpage/Contents.swift
+++ Playground/RxSwiftExtPlayground.playground/Pages/UIViewPropertyAnimator+Rx.xcplaygroundpage/Contents.swift
@@ -18,11 +18,12 @@ import UIKit
 /*:
  ## animate
 
- The `animate` operators provide a Completable that when subscribed to starts the animation (after a delay or not) and completes once the animation is ended
- Please, active the assistant editor to see the preview
+ The `animate` operator provides a Completable that triggers the animation upon subscription and completes when the animation ends.
+
+ Please open the Assistant Editor (⌘⌥⏎) to see the Interactive Live View example.
  */
 
-class AnimateViewController : UIViewController {
+class AnimateViewController: UIViewController {
 
     let disposeBag = DisposeBag()
 
@@ -49,50 +50,54 @@ class AnimateViewController : UIViewController {
         button.setTitle("Play animation", for: .normal)
         button.setTitleColor(.blue, for: .normal)
         button.backgroundColor = .white
-        button.rx.tap.subscribe(onNext: { [unowned self] (_) in
-            self.animate()
-        }).disposed(by: self.disposeBag)
+        button.layer.borderColor = UIColor.black.cgColor
+        button.layer.borderWidth = 2
+
         return button
     }()
 
     lazy var animator1 = {
-        UIViewPropertyAnimator(duration: 0.5, curve: .linear) { [unowned self] in
+        UIViewPropertyAnimator(duration: 0.3, curve: .easeInOut) { [unowned self] in
             self.box1.transform = CGAffineTransform(translationX: 0, y: -100)
         }
     }()
 
     lazy var animator2 = {
-        UIViewPropertyAnimator(duration: 0.3, curve: .linear) { [unowned self] in
-            self.box2.transform = CGAffineTransform(translationX: 0, y: -100).scaledBy(x: 1.2, y: 1.2)
+        UIViewPropertyAnimator(duration: 0.25, curve: .easeInOut) { [unowned self] in
+            self.box2.transform = CGAffineTransform(translationX: 0, y: -100)
+                                    .scaledBy(x: 1.2, y: 1.2)
         }
     }()
 
     lazy var animator3 = {
-        UIViewPropertyAnimator(duration: 0.2, curve: .linear) { [unowned self] in
-            self.box3.transform = CGAffineTransform(translationX: 0, y: -100).rotated(by: CGFloat(M_PI))
+        UIViewPropertyAnimator(duration: 0.15, curve: .easeInOut) { [unowned self] in
+            self.box3.transform = CGAffineTransform(translationX: 0, y: -100)
+                                    .rotated(by: CGFloat(M_PI))
         }
     }()
-
-    private func animate () {
-        // trigger the animation chain
-        self.animator1.rx.animate()
-            .andThen(self.animator2.rx.animate(afterDelay: 0.2))
-            .andThen(self.animator3.rx.animate(afterDelay: 0.1))
-            .subscribe()
-            .disposed(by: self.disposeBag)
-    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // construct the main view
-        self.view.backgroundColor = .white
-        self.view.addSubview(self.box1)
-        self.view.addSubview(self.box2)
-        self.view.addSubview(self.box3)
-        self.view.addSubview(self.button)
-    }
+        let views = [box1, box2, box3, button]
+        view.backgroundColor = .white
 
+        views.forEach {
+            view.addSubview($0)
+        }
+
+        // Trigger chained animations after a button tap
+        button.rx.tap
+            .flatMap { [unowned self] in
+                self.animator1.rx.animate()
+                    .andThen(self.animator2.rx.animate(afterDelay: 0.2))
+                    .andThen(self.animator3.rx.animate(afterDelay: 0.1))
+                    .debug("animation sequence")
+            }
+            .subscribe()
+            .disposed(by: disposeBag)
+    }
 }
 
 // Present the view controller in the Live View window
