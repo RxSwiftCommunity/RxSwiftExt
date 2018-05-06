c26b5c72960a05b33877bc7675b7cecb760dd70e
Joan Disho <dishojoan@gmail.com>
2018-05-06 02:17:29 +0200

    create mapMany operator

---
+++ Source/RxSwift/mapMany.swift
@@ -0,0 +1,25 @@
+//
+//  mapMany.swift
+//  RxSwiftExt
+//
+//  Created by Joan Disho on 06.05.18.
+//  Copyright © 2018 RxSwiftCommunity. All rights reserved.
+//
+
+import RxSwift
+
+extension ObservableType where E: Collection {
+
+    /**
+     Projects elements of each element of an observable collection into a new form.
+     - parameter transform: A transform function to apply to each source element.
+     - returns: An observable collection whose elements are the result of invoking the transform function on each element of source.
+     */
+
+    func mapMany<T>(_ transform: @escaping (E.Element) -> T) -> Observable<[T]> {
+        return map { collection -> [T] in
+            collection.map(transform)
+        }
+    }
+
+}
