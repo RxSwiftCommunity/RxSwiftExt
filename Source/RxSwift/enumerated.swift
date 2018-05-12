7028bcce3803d88ddd25afdace14adb1a67f4018
Joan Disho <dishojoan@gmail.com>
2018-05-12 10:08:29 +0200

    create enumerated observable.

---
+++ Source/RxSwift/enumerated.swift
@@ -0,0 +1,27 @@
+//
+//  enumerated.swift
+//  RxSwiftExt
+//
+//  Created by Joan Disho on 12.05.18.
+//  Copyright © 2018 RxSwiftCommunity. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+extension Observable {
+    /**
+     Converts an Observable into another Observable that also emits the index count of the emission.
+
+     - returns: An enumerated Observable.
+     */
+    func enumerated() -> Observable<(index: Int, element: E)> {
+        let initial: (index: Int, element: E?) = (-1, nil)
+        return scan(initial) { accumulator, element in
+            return (accumulator.index + 1, element)
+            }
+            .map { index, element in
+                return (index: index, element: element!)
+            }
+    }
+}
