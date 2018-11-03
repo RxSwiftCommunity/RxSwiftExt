5c367c5fe18d344ed25facb210dd7dbd7006f267
touyu <dev.akkey0222@gmail.com>
2018-11-03 20:27:46 +0900

    Add Comment

--- Source/RxSwift/flatMapMany.swift
+++ Source/RxSwift/flatMapMany.swift
@@ -9,8 +9,13 @@
 import RxSwift
 
 extension ObservableType where E: Collection {
+    /**
+     Projects each element of an observable collection to an observable collection and merges the resulting observable collections into one observable collection.
 
-    func flatMapMany<O>(_ selector: @escaping (E.Element) throws -> O) -> Observable<[O.E]> where O: ObservableConvertibleType {
+     - parameter selector: A transform function to apply to each element.
+     - returns: An observable sequence whose elements are the result of invoking the one-to-many transform function on each element of the input sequence.
+     */
+    public func flatMapMany<O>(_ selector: @escaping (E.Element) throws -> O) -> Observable<[O.E]> where O: ObservableConvertibleType {
         return flatMap { collection -> Observable<[O.E]> in
             let new = try collection.map(selector)
             return Observable.from(new).merge().toArray()
