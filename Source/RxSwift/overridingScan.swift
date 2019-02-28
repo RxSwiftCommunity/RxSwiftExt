0aaeb9ca8a826bc896597598cb5bf56ea02500c0
Sergey Smagleev <sergey@door2door.io>
2019-02-28 10:56:46 +0100

    Add more tests; Add description; Fix naming;

--- Source/RxSwift/overridingScan.swift
+++ Source/RxSwift/overridingScan.swift
@@ -9,22 +9,34 @@
 import RxSwift
 
 private enum OverridingScanValue<T, U> {
-    case ongoing(T)
+    case sequential(T)
     case overriding(U)
 }
 
 extension ObservableType {
 
+    /**
+     Applies an accumulator function over an observable sequence and returns each intermediate result that can be overridden by values from another observable.
+     
+     The behavior is identical to `scan`, except the result of applying the accumulator function can be overridden by an arbitrary value from another observable.
+     
+     - seealso: [scan operator on reactivex.io](http://reactivex.io/documentation/operators/scan.html)
+     
+     - parameter overridingObservable: An observable which can override the current result of the accumulator function.
+     - parameter seed: The initial accumulator value.
+     - parameter accumulator: An accumulator function to be invoked on each element.
+     - returns: An observable sequence containing the accumulated values.
+     */
     public func overridingScan<T, U: ObservableType>(_ overridingObservable: U,
                                                      _ seed: T,
                                                      accumulator: @escaping (T, Self.E) throws -> T )
         -> Observable<T> where U.E == T {
             return Observable.merge(
-                self.map { OverridingScanValue<Self.E, T>.ongoing($0) },
+                self.map { OverridingScanValue<Self.E, T>.sequential($0) },
                 overridingObservable.map { OverridingScanValue<Self.E, T>.overriding($0) })
                 .scan(seed, accumulator: { (total, current) -> T in
                     switch current {
-                    case .ongoing(let value):
+                    case .sequential(let value):
                         return try accumulator(total, value)
                     case .overriding(let value):
                         return value
--- Tests/RxSwift/OverridingScanTests.swift
+++ Tests/RxSwift/OverridingScanTests.swift
@@ -12,37 +12,59 @@ import RxTest
 
 class OverridingScanTests: XCTestCase {
 
-    func testOverridingScan() {
+    func testOverridingScanInt() {
         let scheduler = TestScheduler(initialClock: 0)
-
         let overridingObservable = scheduler.createHotObservable([
-            next(250, 1),
             next(400, 10),
             completed(500)
             ])
-
         let observable = scheduler.createHotObservable([
             next(300, 2),
             next(350, 3),
             next(450, 4),
-            completed(500)
+            completed(550)
             ])
-
         let res = scheduler.start {
             return observable.overridingScan(overridingObservable, 0) { (total, current) -> Int in
                 return total + current
             }
         }
-
         let correct = [
-            next(250, 1),
-            next(300, 3),
-            next(350, 6),
+            next(300, 2),
+            next(350, 5),
             next(400, 10),
             next(450, 14),
-            completed(500)
+            completed(550)
         ]
+        XCTAssertEqual(res.events, correct)
+    }
 
+    func testOverridingScanArray() {
+        let scheduler = TestScheduler(initialClock: 0)
+        let overridingObservable = scheduler.createHotObservable([
+            next(250, [1]),
+            next(400, [2, 4]),
+            completed(550)
+            ])
+        let observable = scheduler.createHotObservable([
+            next(300, [3, 6]),
+            next(350, [7]),
+            next(450, [22, 8]),
+            completed(500)
+            ])
+        let res = scheduler.start {
+            return observable.overridingScan(overridingObservable, []) { (total, current) -> [Int] in
+                return total + current
+            }
+        }
+        let correct = [
+            next(250, [1]),
+            next(300, [1, 3, 6]),
+            next(350, [1, 3, 6, 7]),
+            next(400, [2, 4]),
+            next(450, [2, 4, 22, 8]),
+            completed(550)
+        ]
         XCTAssertEqual(res.events, correct)
     }
 
