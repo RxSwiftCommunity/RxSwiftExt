4e7e443767d7a45023c8bf95440cbcb7b9c79e6e
Anton Nazarov <35256133+MortyMerr@users.noreply.github.com>
2019-09-04 00:29:35 -0700

    Added `mapTo` for `Single` and `Maybe` (#221)

--- Source/RxSwift/mapTo.swift
+++ Source/RxSwift/mapTo.swift
@@ -17,7 +17,31 @@ extension ObservableType {
 	- parameter value: A constant that each element of the input sequence is being replaced with
 	- returns: An observable sequence containing the values `value` provided as a parameter
 	*/
-	public func mapTo<R>(_ value: R) -> Observable<R> {
+	public func mapTo<Result>(_ value: Result) -> Observable<Result> {
 		return map { _ in value }
 	}
+}
+
+extension PrimitiveSequence where Trait == SingleTrait {
+    /**
+     Returns a Single which success event is mapped to constant provided as a parameter
+
+     - parameter value: A constant that element of the input sequence is being replaced with
+     - returns: A Single containing the value `value` provided as a parameter in case of success
+     */
+    public func mapTo<Result>(_ value: Result) -> Single<Result> {
+        return map { _ in value }
+    }
+}
+
+extension PrimitiveSequence where Trait == MaybeTrait {
+    /**
+     Returns a Maybe which success event is mapped to constant provided as a parameter
+
+     - parameter value: A constant that element of the input sequence is being replaced with
+     - returns: A Maybe containing the value `value` provided as a parameter in case of success
+     */
+    public func mapTo<Result>(_ value: Result) -> Maybe<Result> {
+        return map { _ in value }
+    }
 }
