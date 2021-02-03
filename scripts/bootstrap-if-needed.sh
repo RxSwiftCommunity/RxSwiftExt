3a85a82badb238297c2fb010e3b53449fc959e3a
Anton Plebanovich <anton.plebanovich@gmail.com>
2021-02-03 15:27:34 +0300

    - Carthage build fix - Carthage rebuild on Swift version change

--- scripts/bootstrap-if-needed.sh
+++ scripts/bootstrap-if-needed.sh
@@ -1,10 +1,24 @@
 #!/bin/sh
 
+set -e
+
 RED='\033[1;31m'
 GREEN='\033[1;32m'
 NC='\033[0m' # No Color
 
-if ! cmp -s Cartfile.resolved Carthage/Cartfile.resolved; then
+# Get previous checksum
+mkdir -p "Carthage"
+touch "Carthage/cartSum.txt"
+if [ ! -f "Carthage/cartSum.txt" ]; then
+    prevSum="null";
+else
+    prevSum=`cat Carthage/cartSum.txt`;
+fi
+
+# Get checksum
+cartSum=`{ cat Cartfile.resolved; xcrun swift -version; } | md5`
+
+if [ "$prevSum" != "$cartSum" ] || [ ! -d "Carthage/Build/iOS" ]; then
   printf "${RED}Dependencies out of date with cache.${NC} Bootstrapping...\n"
   scripts/bootstrap.sh
 else
