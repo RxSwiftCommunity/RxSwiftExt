9e75b3cb50f15fd4288079c5cc00fed8900014a1
Anton Plebanovich <anton.plebanovich@gmail.com>
2021-02-03 18:24:02 +0300

    - Removed cache build usage

--- scripts/bootstrap-if-needed.sh
+++ scripts/bootstrap-if-needed.sh
@@ -6,21 +6,32 @@ RED='\033[1;31m'
 GREEN='\033[1;32m'
 NC='\033[0m' # No Color
 
+checksum_file="Carthage/cartSum.txt"
+
+# Computes current Carthage checksum using 'Cartfile.resolved' file, Swift version and checksum version.
+computeChecksum() {
+    local version="1"
+    { cat Cartfile.resolved; xcrun swift -version; echo "${version}" } | md5
+}
+
 # Get previous checksum
 mkdir -p "Carthage"
-touch "Carthage/cartSum.txt"
-if [ ! -f "Carthage/cartSum.txt" ]; then
+touch "${checksum_file}"
+if [ ! -f "${checksum_file}" ]; then
     prevSum="null";
 else
-    prevSum=`cat Carthage/cartSum.txt`;
+    prevSum=`computeChecksum`;
 fi
 
 # Get checksum
-cartSum=`{ cat Cartfile.resolved; xcrun swift -version; } | md5`
+cartSum=`computeChecksum`
 
 if [ "$prevSum" != "$cartSum" ] || [ ! -d "Carthage/Build/iOS" ]; then
   printf "${RED}Dependencies out of date with cache.${NC} Bootstrapping...\n"
   scripts/bootstrap.sh
+
+  echo `computeChecksum` > "${checksum_file}"
+
 else
   printf "${GREEN}Cache up-to-date.${NC} Skipping bootstrap...\n"
 fi
