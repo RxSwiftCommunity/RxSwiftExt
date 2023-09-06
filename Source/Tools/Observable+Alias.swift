eb06cabc0c44ccc2412c3b9f0664f568d4d0341b
Adam Sousa <adam.sousa@emerson.com>
2023-09-06 10:17:12 -0400

    Added Observable+Alias.swift to project file with correct targets.

--- Source/Tools/Observable+Alias.swift
+++ Source/Tools/Observable+Alias.swift
@@ -1,8 +1,9 @@
 //
-//  File.swift
-//  
+//  Observable+Alias.swift
+//  RxSwiftExt
 //
 //  Created by Shai Mishali on 23/08/2023.
+//  Copyright © 2017 RxSwift Community. All rights reserved.
 //
 
 import RxSwift
